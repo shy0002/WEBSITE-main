8aad0608b60783338a76842fa2c45442d464de4f
shy <1529110749@qq.com>
2023-12-04 16:37:56 +0800

    提交测试

---
+++ chat-server/src/main/java/com/ayang/website/Main.java
@@ -0,0 +1,11 @@
+package com.ayang.website;
+
+/**
+ * @author 盛浩洋
+ * @date 2023/12/4 13:41
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
