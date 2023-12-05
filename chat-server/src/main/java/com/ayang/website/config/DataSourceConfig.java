6ae0b531a83a136316e5e52f45c0fa69d215d529
shy <1529110749@qq.com>
2023-12-05 15:56:52 +0800

    数据源配置类

---
+++ chat-server/src/main/java/com/ayang/website/config/DataSourceConfig.java
@@ -0,0 +1,17 @@
+package com.ayang.website.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author shy
+ * @date 2023/12/5
+ * @description DataSourceConfig
+ */
+@Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
+@MapperScan({"com.ayang.website.**.mapper"})
+public class DataSourceConfig {
+}
