46b678fa2a93f0597b87f846e0b82fb7640aaeac
shy <1529110749@qq.com>
2023-12-05 09:11:45 +0800

    修改添加启动类

---
+++ chat-server/src/main/java/com/ayang/website/ChatCustomApplication.java
@@ -0,0 +1,25 @@
+package com.ayang.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 盛浩洋
+ * @date 2023/12/4 13:41
+ */
+@SpringBootApplication(scanBasePackages = {"com.ayang.website"})
+public class ChatCustomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatCustomApplication.class,args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
