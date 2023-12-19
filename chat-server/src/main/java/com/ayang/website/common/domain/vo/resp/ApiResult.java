43a4af69f3967acdbf5122b6c7d57bec35da1479
shy <1529110749@qq.com>
2023-12-19 14:06:50 +0800

    通用请求框架

---
+++ chat-server/src/main/java/com/ayang/website/common/domain/vo/resp/ApiResult.java
@@ -0,0 +1,50 @@
+package com.ayang.website.common.domain.vo.resp;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @description: 通用返回体
+ * @author: <a href="https://github.com/shy0002“>ayang</a>
+ * @date: 2023/12/19
+ **/
+@Data
+@ApiModel("基础返回体")
+public class ApiResult<T> {
+    @ApiModelProperty("成功标识true or false")
+    private Boolean success;
+    @ApiModelProperty("错误码")
+    private Integer errCode;
+    @ApiModelProperty("错误消息")
+    private String errMsg;
+    @ApiModelProperty("返回对象")
+    private T data;
+
+    public static <T> ApiResult<T> success() {
+        ApiResult<T> result = new ApiResult<T>();
+        result.setData(null);
+        result.setSuccess(Boolean.TRUE);
+        return result;
+    }
+
+    public static <T> ApiResult<T> success(T data) {
+        ApiResult<T> result = new ApiResult<T>();
+        result.setData(data);
+        result.setSuccess(Boolean.TRUE);
+        return result;
+    }
+
+    public static <T> ApiResult<T> fail(Integer code, String msg) {
+        ApiResult<T> result = new ApiResult<T>();
+        result.setSuccess(Boolean.FALSE);
+        result.setErrCode(code);
+        result.setErrMsg(msg);
+        return result;
+    }
+
+
+    public boolean isSuccess() {
+        return this.success;
+    }
+}
