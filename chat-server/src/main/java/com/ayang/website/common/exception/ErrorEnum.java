25aa53324fa7bd556eced03eddbb6d74cc5997fb
shy <1529110749@qq.com>
2023-12-20 17:23:13 +0800

    错误枚举基础接口

---
+++ chat-server/src/main/java/com/ayang/website/common/exception/ErrorEnum.java
@@ -0,0 +1,12 @@
+package com.ayang.website.common.exception;
+
+/**
+ * @author shy
+ * @date 2023/12/20
+ * @description ErrorEnum
+ */
+public interface ErrorEnum {
+    Integer getErrorCode();
+
+    String getErrorMsg();
+}
