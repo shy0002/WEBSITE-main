672c46a1217346114549a19c91cec7321553bc2b
shy <1529110749@qq.com>
2023-12-20 17:25:48 +0800

    修改为数据库交互服务，添加根据物品类型获取物品列表的功能

--- chat-server/src/main/java/com/ayang/website/user/dao/ItemConfigDao.java
+++ chat-server/src/main/java/com/ayang/website/user/dao/ItemConfigDao.java
@@ -3,17 +3,24 @@ package com.ayang.website.user.dao;
 import com.ayang.website.user.domain.entity.ItemConfig;
 import com.ayang.website.user.mapper.ItemConfigMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
- * 功能物品配置表 服务实现类
+ * 功能物品配置表 数据库交互服务
  * </p>
  *
  * @author <a href="https://github.com/shy0002">ayang</a>
  * @since 2023-12-19
  */
-@Service
+@Repository
 public class ItemConfigDao extends ServiceImpl<ItemConfigMapper, ItemConfig> {
 
+    public List<ItemConfig> getByType(Integer itemType) {
+        return lambdaQuery()
+                .eq(ItemConfig::getType, itemType)
+                .list();
+    }
 }
