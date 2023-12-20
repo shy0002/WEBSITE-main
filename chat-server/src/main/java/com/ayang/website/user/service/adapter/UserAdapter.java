f4c3a7085adb42f02450c20d30a7f0b8f2e714bb
shy <1529110749@qq.com>
2023-12-20 17:26:26 +0800

    添加徽章列表返回适配器类

--- chat-server/src/main/java/com/ayang/website/user/service/adapter/UserAdapter.java
+++ chat-server/src/main/java/com/ayang/website/user/service/adapter/UserAdapter.java
@@ -1,9 +1,19 @@
 package com.ayang.website.user.service.adapter;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.ayang.website.common.domain.enums.YesOrNoEnum;
+import com.ayang.website.user.domain.entity.ItemConfig;
 import com.ayang.website.user.domain.entity.User;
+import com.ayang.website.user.domain.entity.UserBackpack;
+import com.ayang.website.user.domain.vo.resp.BadgeResp;
 import com.ayang.website.user.domain.vo.resp.UserInfoResp;
 import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author shy
@@ -29,5 +39,18 @@ public class UserAdapter {
         BeanUtil.copyProperties(user, vo);
         vo.setModifyNameChance(modifyNameCount);
         return vo;
+    }
+
+    public static List<BadgeResp> buildBadgeResp(List<ItemConfig> itemConfigs, List<UserBackpack> backpacks, User user) {
+        Set<Long> obtainItemSet = backpacks.stream().map(UserBackpack::getItemId).collect(Collectors.toSet());
+        return itemConfigs.stream().map(a -> {
+                    BadgeResp resp = new BadgeResp();
+                    BeanUtil.copyProperties(a, resp);
+                    resp.setObtain(obtainItemSet.contains(a.getId()) ? YesOrNoEnum.YES.getStatus() : YesOrNoEnum.NO.getStatus());
+                    resp.setWearing(Objects.equals(a.getId(), user.getItemId()) ? YesOrNoEnum.YES.getStatus() : YesOrNoEnum.NO.getStatus());
+                    return resp;
+                }).sorted(Comparator.comparing(BadgeResp::getWearing, Comparator.reverseOrder())
+                        .thenComparing(BadgeResp::getObtain, Comparator.reverseOrder()))
+                .collect(Collectors.toList());
     }
 }
