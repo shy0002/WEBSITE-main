5ad4f06f0e8c312174e9ca1a8a7d9d7593b56efe
shy <1529110749@qq.com>
2023-12-20 17:22:23 +0800

    新增基本错误枚举类

---
+++ chat-server/src/main/java/com/ayang/website/common/exception/CommonErrorEnum.java
@@ -0,0 +1,30 @@
+package com.ayang.website.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author shy
+ * @date 2023/12/20
+ * @description CommonErrorEnum
+ */
+@AllArgsConstructor
+@Getter
+public enum CommonErrorEnum implements ErrorEnum{
+    BUSINESS_ERROR(0, "{0}"),
+    SYSTEM_ERROR(-1, "系统出小差了，请稍后再试~"),
+    PARAM_INVALID(-2, "参数校验失败"),
+    ;
+    private final Integer code;
+    private final String msg;
+
+    @Override
+    public Integer getErrorCode() {
+        return code;
+    }
+
+    @Override
+    public String getErrorMsg() {
+        return msg;
+    }
+}
