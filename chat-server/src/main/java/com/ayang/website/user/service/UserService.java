9bfe476c8ae6b57ebe5bf1b54780b4eba18b9a8a
shy <1529110749@qq.com>
2023-12-20 17:29:05 +0800

    完善注解，添加修改名称、获取所有徽章列表和佩戴徽章的接口

--- chat-server/src/main/java/com/ayang/website/user/service/UserService.java
+++ chat-server/src/main/java/com/ayang/website/user/service/UserService.java
@@ -1,8 +1,10 @@
 package com.ayang.website.user.service;
 
 import com.ayang.website.user.domain.entity.User;
-import com.ayang.website.user.domain.vo.req.ModifyNameReq;
+import com.ayang.website.user.domain.vo.resp.BadgeResp;
 import com.ayang.website.user.domain.vo.resp.UserInfoResp;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,14 +16,43 @@ import com.ayang.website.user.domain.vo.resp.UserInfoResp;
  */
 public interface UserService {
 
-    Long register(User insert);
+    /**
+     * 用户注册
+     *
+     * @param user 用户信息
+     * @return 用户id
+     */
+    Long register(User user);
 
     /**
      * 获取用户信息
+     *
      * @param uid 用户id
      * @return 用户信息响应
      */
     UserInfoResp getUserInfo(Long uid);
 
-    void cmodifyName(Long uid, ModifyNameReq req);
+    /**
+     * 修改名称
+     *
+     * @param uid  用户id
+     * @param name 新名称
+     */
+    void modifyName(Long uid, String name);
+
+    /**
+     * 获取所有的徽章列表
+     *
+     * @param uid 用户id
+     * @return 徽章信息列表
+     */
+    List<BadgeResp> badges(Long uid);
+
+    /**
+     * 佩戴徽章
+     *
+     * @param uid    用户id
+     * @param itemId 徽章id
+     */
+    void wearingBadge(Long uid, Long itemId);
 }
