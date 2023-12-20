6cbdd9d54465cdd9cced201f762bad9d6025fcb8
shy <1529110749@qq.com>
2023-12-20 17:28:31 +0800

    修改为数据库交互类，添加根据名称获取用户、修改用户名和佩戴勋章的功能

--- chat-server/src/main/java/com/ayang/website/user/dao/UserDao.java
+++ chat-server/src/main/java/com/ayang/website/user/dao/UserDao.java
@@ -3,20 +3,38 @@ package com.ayang.website.user.dao;
 import com.ayang.website.user.domain.entity.User;
 import com.ayang.website.user.mapper.UserMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
- * 用户表 服务实现类
+ * 用户表 数据库交互服务
  * </p>
  *
  * @author shy
  * @since 2023-12-05
  */
-@Service
+@Repository
 public class UserDao extends ServiceImpl<UserMapper, User> {
 
     public User getByOpenId(String openId) {
         return lambdaQuery().eq(User::getOpenId, openId).one();
+    }
+
+    public User getByName(String name) {
+        return lambdaQuery().eq(User::getName, name).one();
+    }
+
+    public void modifyName(Long uid, String name) {
+        lambdaUpdate()
+                .eq(User::getId, uid)
+                .set(User::getName, name)
+                .update();
+    }
+
+    public void wearingBadge(Long uid, Long itemId) {
+        lambdaUpdate()
+                .eq(User::getId, uid)
+                .set(User::getItemId, itemId)
+                .update();
     }
 }
