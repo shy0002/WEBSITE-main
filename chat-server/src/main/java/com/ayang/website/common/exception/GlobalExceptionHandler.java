53ddae9e660125ea7839a797e85ab3c77ac17156
shy <1529110749@qq.com>
2023-12-20 17:24:22 +0800

    完善请求参数校验报错捕获，添加业务异常捕获和系统异常捕获

--- chat-server/src/main/java/com/ayang/website/common/exception/GlobalExceptionHandler.java
+++ chat-server/src/main/java/com/ayang/website/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ayang.website.common.exception;
 
 import com.ayang.website.common.domain.vo.resp.ApiResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +12,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @description 全局异常捕获程序
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ApiResult<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
-        return ApiResult.fail(null, null);
+        StringBuilder errorMsg = new StringBuilder();
+        e.getBindingResult().getFieldErrors().forEach(x -> errorMsg.append(x.getField()).append(x.getDefaultMessage()).append(","));
+        errorMsg.setLength(errorMsg.length() - 1);
+        return ApiResult.fail(CommonErrorEnum.PARAM_INVALID.getCode(), errorMsg.toString());
+    }
+
+    /**
+     * 业务异常
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    public ApiResult<?> businessException(BusinessException e){
+        log.info("business exception! The reason is:{}", e.getMessage());
+        return ApiResult.fail(e.getErrorCode(), e.getErrorMsg());
+    }
+
+    /**
+     * 最后一道防线
+     * @param e 错误
+     * @return 返回框架
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public ApiResult<?> throwable(Throwable e){
+        log.error("system exception! The reason is:{}", e.getMessage(), e);
+        return ApiResult.fail(CommonErrorEnum.SYSTEM_ERROR);
     }
 }
