3bda4711e920fbce3fbabb7eac93d03456737570
shy <1529110749@qq.com>
2023-12-20 17:27:32 +0800

    修改为数据库交互类，添加获取物品功能、使用物品功能和根据物品id列表获取背包中物品的功能

--- chat-server/src/main/java/com/ayang/website/user/dao/UserBackpackDao.java
+++ chat-server/src/main/java/com/ayang/website/user/dao/UserBackpackDao.java
@@ -4,17 +4,19 @@ import com.ayang.website.common.domain.enums.YesOrNoEnum;
 import com.ayang.website.user.domain.entity.UserBackpack;
 import com.ayang.website.user.mapper.UserBackpackMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
- * 用户背包表 服务实现类
+ * 用户背包表 数据库交互服务
  * </p>
  *
  * @author <a href="https://github.com/shy0002">ayang</a>
  * @since 2023-12-19
  */
-@Service
+@Repository
 public class UserBackpackDao extends ServiceImpl<UserBackpackMapper, UserBackpack> {
 
     public Integer getCountByValidItemId(Long uid, Long itemId) {
@@ -23,5 +25,31 @@ public class UserBackpackDao extends ServiceImpl<UserBackpackMapper, UserBackpac
                 .eq(UserBackpack::getItemId, itemId)
                 .eq(UserBackpack::getStatus, YesOrNoEnum.NO.getStatus())
                 .count();
+    }
+
+    public UserBackpack getFirstValidItem(Long uid, Long itemId) {
+        return lambdaQuery().eq(UserBackpack::getUid, uid)
+                .eq(UserBackpack::getItemId, itemId)
+                .eq(UserBackpack::getStatus, YesOrNoEnum.NO.getStatus())
+                .orderByAsc(UserBackpack::getId)
+                .last("limit 1")
+                .one();
+
+    }
+
+    public boolean useItem(UserBackpack item) {
+        return lambdaUpdate()
+                .eq(UserBackpack::getItemId, item.getId())
+                .eq(UserBackpack::getStatus, YesOrNoEnum.NO.getStatus())
+                .set(UserBackpack::getStatus, YesOrNoEnum.YES.getStatus())
+                .update();
+    }
+
+    public List<UserBackpack> getByItemId(Long uid, List<Long> itemIds) {
+        return lambdaQuery()
+                .eq(UserBackpack::getUid, uid)
+                .eq(UserBackpack::getStatus, YesOrNoEnum.NO.getStatus())
+                .in(UserBackpack::getItemId, itemIds)
+                .list();
     }
 }
